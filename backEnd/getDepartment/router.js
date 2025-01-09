5d1ca795886a7d04ec51273704a8f7139cab6917
Hai La <laduchai20111995@gmail.com>
2025-01-09 17:10:18 +0700

    9-1-2025-1

--- backEnd/getDepartment/router.js
+++ backEnd/getDepartment/router.js
@@ -20,30 +20,30 @@ const service = process.env.SERVICE;
 
 
 
-// router.get('/readAllDepartmentGroupWithFK', 
-//     Authentication_SHOPM,
-//     (req, res) => {
-//     const uuid_provider = req.query.uuid_provider;
-//     departmentGroupCRUD.readAllWithFK(uuid_provider, (departmentGroupAll, err) => {
-//         if (err) {
-//             logEvents(`${req.url}---${req.method}---${err}`);
-//             return res.status(500).send(err);
-//         } else {
-//             if (departmentGroupAll && departmentGroupAll!==null) {
-//                 return res.status(200).json({
-//                     departmentGroupAll: departmentGroupAll,
-//                     success: true,
-//                     message: `departmentGroupAll is read successly (${service}) !`
-//                 });
-//             } else {
-//                 return res.status(200).json({
-//                     departmentGroupAll: departmentGroupAll,
-//                     success: false,
-//                     message: `departmentGroupAll is NOT read successly (${service}) !`
-//                 })
-//             }
-//         }
-//     })
-// })
+router.get('/readAllDepartmentGroupWithFK', 
+    Authentication_SHOPM,
+    (req, res) => {
+    const uuid_provider = req.query.uuid_provider;
+    departmentGroupCRUD.readAllWithFK(uuid_provider, (departmentGroupAll, err) => {
+        if (err) {
+            logEvents(`${req.url}---${req.method}---${err}`);
+            return res.status(500).send(err);
+        } else {
+            if (departmentGroupAll && departmentGroupAll!==null) {
+                return res.status(200).json({
+                    departmentGroupAll: departmentGroupAll,
+                    success: true,
+                    message: `departmentGroupAll is read successly (${service}) !`
+                });
+            } else {
+                return res.status(200).json({
+                    departmentGroupAll: departmentGroupAll,
+                    success: false,
+                    message: `departmentGroupAll is NOT read successly (${service}) !`
+                })
+            }
+        }
+    })
+})
 
 module.exports = router;
